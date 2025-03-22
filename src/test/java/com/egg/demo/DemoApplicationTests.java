ccb6de2c7fbd481119e4a44698d904ce4b40fa92
cindygnaedinger <100149373+cindygnaedinger@users.noreply.github.com>
2025-03-22 11:07:06 -0300

    Crea proyecto Spring

---
+++ src/test/java/com/egg/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
